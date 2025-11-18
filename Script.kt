97cd263c5f8b18327f503b9579a682cfccf0daac
Francis Junior <francisjunior@uniplaclages.edu.br>
2025-11-18 15:59:21 -0300

    Codigo Melhorado ( issue #3 )

--- Script.kt
+++ Script.kt
@@ -1,9 +1,21 @@
 fun main() {
-    println("Olá, Kotlin!")
-
+    // 1. Definição dos dados
     val nome = "Juninho"
-    var idade = 18
+    val idade = 19
 
+    // 2. Chamada da função para exibir os dados
+    exibirInformacoes(nome, idade)
+}
+
+/**
+ * Imprime uma saudação inicial e exibe o nome e a idade da pessoa.
+ *
+ * @param nome O nome da pessoa.
+ * @param idade A idade da pessoa.
+ */
+fun exibirInformacoes(nome: String, idade: Int) {
+    println("Olá, Kotlin!") // A saudação fica separada
+    println("---")
     println("Nome: $nome")
     println("Idade: $idade")
 }
